8cdf346c29e6dab9e458c04dc3eed1a4de37161a
TSKHayagrevaC <srikanth.thiru12@gmail.com>
2023-11-01 12:07:46 +0530

    Adde not found route for edit invoice page.

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,19 +1,17 @@
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import Form from '@/app/ui/invoices/edit-form';
-import { InvoiceForm } from '@/app/lib/definitions';
+import { notFound } from 'next/navigation';
  
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
-  ]);  
-  const sudoInvoice: InvoiceForm = {
-    amount: 100,
-    id: 'id',
-    customer_id: 'customer_id',
-    status: 'pending'
+  ]);
+
+  if (!invoice) {
+    notFound();
   }
 
   return (
@@ -28,7 +26,7 @@ export default async function Page({ params }: { params: { id: string } }) {
           },
         ]}
       />
-      <Form invoice={invoice ?? sudoInvoice} customers={customers} />
+      <Form invoice={invoice} customers={customers} />
     </main>
   );
 }
