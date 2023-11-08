6aa8d126089bb11aa113c172dae6a2c024f9ef9d
TSKHayagrevaC <srikanth.thiru12@gmail.com>
2023-11-08 14:54:08 +0530

    fix: navigate authenticated user to the previous url.

--- auth.config.ts
+++ auth.config.ts
@@ -6,14 +6,16 @@ export const authConfig = {
     signIn: '/login',
   },
   callbacks: {
-    authorized({ auth, request: { url, nextUrl } }) {
+    authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const redirectUrl = nextUrl.origin; // Extract the domain from the previous URL
+
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
+        return false; // Redirect unauthenticated users to the login page on the same domain
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', url));
+        return Response.redirect(new URL('/dashboard', redirectUrl)); // Navigate to the dashboard on the same domain
       }
       return true;
     },
