db1f0315a99b3f3da405d20b4a5de85418df0205
TSKHayagrevaC <srikanth.thiru12@gmail.com>
2023-11-08 17:07:35 +0530

    fix: revert to the next js tutorial

--- auth.config.ts
+++ auth.config.ts
@@ -9,13 +9,11 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      const redirectUrl = nextUrl.origin; // Extract the domain from the previous URL
-
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to the login page on the same domain
+        return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', redirectUrl)); // Navigate to the dashboard on the same domain
+        return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
     },
